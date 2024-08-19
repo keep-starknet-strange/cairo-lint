775a37de7545e4ee7dccc4cb88e8883e78878766
Marco Araya Jiménez <marcojime23@gmail.com>
2024-08-19 16:21:50 -0300

    feat: add refactor improvement for lint

--- crates/cairo-lint-core/src/lints/double_parens.rs
+++ crates/cairo-lint-core/src/lints/double_parens.rs
@@ -7,18 +7,13 @@ use cairo_lang_syntax::node::{TypedStablePtr, TypedSyntaxNode};
 pub const DOUBLE_PARENS: &str = "unnecessary double parentheses found. Consider removing them.";
 
 pub fn check_double_parens(db: &dyn SyntaxGroup, expr: &Expr, diagnostics: &mut Vec<PluginDiagnostic>) {
-    let span = match expr {
-        Expr::Parenthesized(parenthesized_expr) => {
-            if let Expr::Parenthesized(_) | Expr::Tuple(_) = parenthesized_expr.expr(db) {
-                Some(parenthesized_expr.as_syntax_node().span(db))
-            } else {
-                None
-            }
-        }
-        _ => None,
+    let is_double_parens = if let Expr::Parenthesized(parenthesized_expr) = expr {
+        matches!(parenthesized_expr.expr(db), Expr::Parenthesized(_) | Expr::Tuple(_))
+    } else {
+        false
     };
 
-    if let Some(_span) = span {
+    if is_double_parens {
         diagnostics.push(PluginDiagnostic {
             stable_ptr: expr.stable_ptr().untyped(),
             message: DOUBLE_PARENS.to_string(),
