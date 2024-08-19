53182e4f0cef434dee5d6f190828ee54b1ae99fc
LucasLvy <lucaslevy10@gmail.com>
2024-08-19 09:57:41 +0200

    refacto(core): switch to semantic query and remove custom db

--- crates/cairo-lint-core/src/lib.rs
+++ crates/cairo-lint-core/src/lib.rs
@@ -1,5 +1,4 @@
 #![feature(let_chains)]
-pub mod db;
 pub mod fix;
 pub mod lints;
 pub mod plugin;
---
+++ crates/cairo-lint-core/src/lints/loops.rs
@@ -0,0 +1,49 @@
+use cairo_lang_defs::plugin::PluginDiagnostic;
+use cairo_lang_diagnostics::Severity;
+use cairo_lang_semantic::db::SemanticGroup;
+use cairo_lang_semantic::{Arenas, Expr, ExprLoop, Statement};
+
+pub const LOOP_MATCH_POP_FRONT: &str =
+    "you seem to be trying to use `loop` for iterating over a span. Consider using `for in`";
+
+const SPAN_MATCH_POP_FRONT: &str = "\"SpanImpl::pop_front\"";
+
+pub fn check_loop_match_pop_front(
+    db: &dyn SemanticGroup,
+    loop_expr: &ExprLoop,
+    diagnostics: &mut Vec<PluginDiagnostic>,
+    arenas: &Arenas,
+) {
+    let Expr::Block(expr_block) = &arenas.exprs[loop_expr.body] else {
+        return;
+    };
+    if let Some(tail) = &expr_block.tail
+        && let Expr::Match(expr_match) = &arenas.exprs[*tail]
+        && let Expr::FunctionCall(func_call) = &arenas.exprs[expr_match.matched_expr]
+    {
+        if func_call.function.name(db) == SPAN_MATCH_POP_FRONT {
+            diagnostics.push(PluginDiagnostic {
+                stable_ptr: loop_expr.stable_ptr.into(),
+                message: LOOP_MATCH_POP_FRONT.to_owned(),
+                severity: Severity::Warning,
+            });
+            return;
+        }
+    }
+    for statement in &expr_block.statements {
+        if let Statement::Expr(stmt_expr) = &arenas.statements[*statement]
+            && let Expr::Match(expr_match) = &arenas.exprs[stmt_expr.expr]
+        {
+            let Expr::FunctionCall(func_call) = &arenas.exprs[expr_match.matched_expr] else {
+                continue;
+            };
+            if func_call.function.name(db) == SPAN_MATCH_POP_FRONT {
+                diagnostics.push(PluginDiagnostic {
+                    stable_ptr: loop_expr.stable_ptr.into(),
+                    message: LOOP_MATCH_POP_FRONT.to_owned(),
+                    severity: Severity::Warning,
+                })
+            }
+        }
+    }
+}
