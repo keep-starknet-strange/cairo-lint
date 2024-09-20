d78dc01ed9bb6d3e1661b0f9015ae391e30fa3bc
BrunoAmbricca <bambricca@gmail.com>
2024-09-20 10:36:43 -0300

    Lint Refactor

--- crates/cairo-lint-core/src/lints/panic.rs
+++ crates/cairo-lint-core/src/lints/panic.rs
@@ -1,8 +1,7 @@
 use cairo_lang_defs::plugin::PluginDiagnostic;
 use cairo_lang_diagnostics::Severity;
 use cairo_lang_semantic::db::SemanticGroup;
-use cairo_lang_semantic::{Arenas, ExprFunctionCall};
-use cairo_lang_semantic::FunctionId;
+use cairo_lang_semantic::{Arenas, Expr, ExprFunctionCall};
 
 pub const PANIC_IN_CODE: &str = "Using `panic!` is discouraged.";
 
@@ -10,24 +9,23 @@ pub fn check_panic_usage(
     db: &dyn SemanticGroup,
     expr_function_call: &ExprFunctionCall,
     diagnostics: &mut Vec<PluginDiagnostic>,
-    _arenas: &Arenas,  
+    arenas: &Arenas,  
 ) {
-    println!("Lint `check_panic_usage` ejecutado");
-    // Obtiene el identificador de la función
-    let function_id: FunctionId = expr_function_call.function;
-    
-    // Obtiene el nombre de la función desde el `FunctionId`
-    let function_name = function_id.name(db);
 
-    println!("Checking function: {}", function_name);
-    
-    // Verifica si el nombre de la función es "panic"
-    if function_name == "panic!" {
-        diagnostics.push(PluginDiagnostic {
-            stable_ptr: expr_function_call.stable_ptr.into(),
-            message: PANIC_IN_CODE.to_owned(),
-            severity: Severity::Warning,
-        });
-        println!("Detected panic! in the code.");
+    for (_expr_id, expr) in &arenas.exprs {
+
+        if let Expr::FunctionCall(function_call) = expr {
+
+            let function_id = function_call.function;
+
+            if function_id.name(db) == "panic" {
+                diagnostics.push(PluginDiagnostic {
+                    stable_ptr: expr_function_call.stable_ptr.into(),
+                    message: PANIC_IN_CODE.to_owned(),
+                    severity: Severity::Warning,
+                });
+            }   
+        }
     }
 }
+
