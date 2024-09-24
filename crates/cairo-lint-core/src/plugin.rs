5f3767896a2a44dd8aa57168b8c35f22d45efe9d
Tuan Tran <tropicaldog17@gmail.com>
2024-09-24 16:47:26 +0700

    fix

--- crates/cairo-lint-core/src/plugin.rs
+++ crates/cairo-lint-core/src/plugin.rs
@@ -1,6 +1,5 @@
 use cairo_lang_defs::ids::{FunctionWithBodyId, ModuleId, ModuleItemId};
 use cairo_lang_defs::plugin::PluginDiagnostic;
-use cairo_lang_semantic::Expr;
 use cairo_lang_semantic::db::SemanticGroup;
 use cairo_lang_semantic::plugin::{AnalyzerPlugin, PluginSuite};
 use cairo_lang_semantic::Expr;
