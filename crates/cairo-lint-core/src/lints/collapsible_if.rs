d819ef1fcaa7e243309b1fc42cf89e30b358c7c3
jimenezz22 <ljimenezbogantes@gmail.com>
2024-09-25 14:08:17 -0600

    feat: remove comments

--- crates/cairo-lint-core/src/lints/collapsible_if.rs
+++ crates/cairo-lint-core/src/lints/collapsible_if.rs
@@ -10,38 +10,31 @@ pub const COLLAPSIBLE_IF: &str =
 pub fn check_collapsible_if(db: &dyn SyntaxGroup, expr_if: &ExprIf, diagnostics: &mut Vec<PluginDiagnostic>) {
     let if_block = expr_if.if_block(db);
 
-    // Verificar si el bloque del if externo contiene solo el if interno
     let statements = if_block.statements(db).elements(db);
     if statements.len() != 1 {
-        // Si hay más de una sentencia, no sugerir la combinación
         return;
     }
 
     if let Some(Statement::Expr(inner_expr_stmt)) = statements.first() {
         if let Expr::If(inner_if_expr) = inner_expr_stmt.expr(db) {
-            // Verificar si el if interno tiene un bloque else
             match inner_if_expr.else_clause(db) {
                 OptionElseClause::Empty(_) => {
-                    // No hay bloque else, podemos continuar
                 }
                 OptionElseClause::ElseClause(_) => {
-                    // Hay un bloque else, no debemos combinar
                     return;
                 }
             }
 
-            // Verificar si el if externo tiene un bloque else
             match expr_if.else_clause(db) {
                 OptionElseClause::Empty(_) => {
-                    // No hay bloque else, podemos continuar
+
                 }
                 OptionElseClause::ElseClause(_) => {
-                    // Hay un bloque else, no debemos combinar
+
                     return;
                 }
             }
 
-            // Si no hay else y el if interno es la única sentencia, sugerir la combinación
             diagnostics.push(PluginDiagnostic {
                 stable_ptr: expr_if.stable_ptr().untyped(),
                 message: COLLAPSIBLE_IF.to_string(),
