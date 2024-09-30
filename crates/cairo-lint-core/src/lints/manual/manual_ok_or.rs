74bd0263c1908b93de5550b032695b9bbf51c5d8
Charlotte <charlotte.leo@hotmail.com>
2024-09-30 16:36:42 +0800

    manual if

--- crates/cairo-lint-core/src/lints/manual/manual_ok_or.rs
+++ crates/cairo-lint-core/src/lints/manual/manual_ok_or.rs
@@ -1,10 +1,10 @@
 use cairo_lang_defs::plugin::PluginDiagnostic;
 use cairo_lang_diagnostics::Severity;
-use cairo_lang_syntax::node::ast::ExprMatch;
+use cairo_lang_syntax::node::ast::{ExprIf, ExprMatch};
 use cairo_lang_syntax::node::db::SyntaxGroup;
 use cairo_lang_syntax::node::TypedSyntaxNode;
 
-use crate::lints::manual::{check_manual, ManualLint};
+use crate::lints::manual::{check_manual, check_manual_if, ManualLint};
 
 pub const MANUAL_OK_OR: &str = "Manual match for Option<T> detected. Consider using ok_or instead";
 
@@ -12,6 +12,16 @@ pub fn check_manual_ok_or(db: &dyn SyntaxGroup, expr_match: &ExprMatch, diagnost
     if check_manual(db, expr_match, ManualLint::ManualOkOr) {
         diagnostics.push(PluginDiagnostic {
             stable_ptr: expr_match.as_syntax_node().stable_ptr(),
+            message: MANUAL_OK_OR.to_owned(),
+            severity: Severity::Warning,
+        });
+    }
+}
+
+pub fn check_manual_if_ok_or(db: &dyn SyntaxGroup, expr_if: &ExprIf, diagnostics: &mut Vec<PluginDiagnostic>) {
+    if check_manual_if(db, expr_if, ManualLint::ManualOkOr) {
+        diagnostics.push(PluginDiagnostic {
+            stable_ptr: expr_if.as_syntax_node().stable_ptr(),
             message: MANUAL_OK_OR.to_owned(),
             severity: Severity::Warning,
         });
