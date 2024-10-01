7c15c4b482d121544cd2ae3e441a7b4b35abedf0
Charlotte <charlotte.leo@hotmail.com>
2024-10-01 16:53:42 +0800

    add doc

--- crates/cairo-lint-core/src/lints/manual/helpers.rs
+++ crates/cairo-lint-core/src/lints/manual/helpers.rs
@@ -4,6 +4,16 @@ use cairo_lang_syntax::node::ast::{
 use cairo_lang_syntax::node::db::SyntaxGroup;
 use cairo_lang_syntax::node::TypedSyntaxNode;
 
+/// Checks if the input statement is a `FunctionCall` then checks if the function name is one of the
+/// func_names input list.
+///
+/// # Arguments
+/// * `statement` - A statement that may contain a `FunctionCall`.
+/// * `db` - Reference to the `SyntaxGroup` for syntax tree access.
+/// * `func_names` - A list of target function names.
+///
+/// # Returns
+/// * `true` if the function name matches any of the input names, otherwise `false`.
 pub fn statement_check_func_name(statement: Statement, db: &dyn SyntaxGroup, func_names: &[&str]) -> bool {
     match statement {
         Statement::Expr(statement_expr) => {
@@ -19,6 +29,15 @@ pub fn statement_check_func_name(statement: Statement, db: &dyn SyntaxGroup, fun
     }
 }
 
+/// Checks if the inner_pattern in the input `Pattern::Enum` matches the given argument name.
+///
+/// # Arguments
+/// * `pattern` - The pattern to check.
+/// * `db` - Reference to the `SyntaxGroup` for syntax tree access.
+/// * `arg_name` - The target name.
+///
+/// # Returns
+/// * `true` if the argument name matches, otherwise `false`.
 pub fn pattern_check_enum_arg(pattern: &Pattern, db: &dyn SyntaxGroup, arg_name: String) -> bool {
     match pattern {
         Pattern::Enum(enum_pattern) => {
@@ -34,6 +53,16 @@ pub fn pattern_check_enum_arg(pattern: &Pattern, db: &dyn SyntaxGroup, arg_name:
     }
 }
 
+/// Checks if the condition of the `ExprIf` expression is a `ConditionLet`, then checks that the
+/// pattern of the expression is an `Enum` and contains an inner pattern that is the same as the
+/// statement in the if block
+///
+/// # Arguments
+/// * `expr` - The ExprIf expression to check.
+/// * `db` - Reference to the `SyntaxGroup` for syntax tree access.
+///
+/// # Returns
+/// * `true` if the pattern matches the if block statement, otherwise `false`.
 pub fn expr_check_inner_pattern_is_if_block_statement(expr: &ExprIf, db: &dyn SyntaxGroup) -> bool {
     if let Condition::Let(condition_let) = expr.condition(db) {
         match &condition_let.patterns(db).elements(db)[0] {
@@ -54,6 +83,15 @@ pub fn expr_check_inner_pattern_is_if_block_statement(expr: &ExprIf, db: &dyn Sy
     }
 }
 
+/// Checks if the input `Expr` is a `FunctionCall` with the specified function name.
+///
+/// # Arguments
+/// * `arm_expression` - The expression to check.
+/// * `db` - Reference to the `SyntaxGroup` for syntax tree access.
+/// * `func_name` - The target function name.
+///
+/// # Returns
+/// * `true` if the expression is a function call and the function name matches, otherwise `false`.
 pub fn arm_expr_check_func_name(arm_expression: Expr, db: &dyn SyntaxGroup, func_name: &str) -> bool {
     if let Expr::FunctionCall(func_call) = arm_expression {
         func_call.path(db).as_syntax_node().get_text(db) == func_name
@@ -62,6 +100,14 @@ pub fn arm_expr_check_func_name(arm_expression: Expr, db: &dyn SyntaxGroup, func
     }
 }
 
+/// Retrieves the else `ExprBlock` from an `OptionElseClause` clause if it's non-empty.
+///
+/// # Arguments
+/// * `else_clause` - The `OptionElseClause` to extract the block from.
+/// * `db` - Reference to the `SyntaxGroup` for syntax tree access.
+///
+/// # Returns
+/// * `Some(ExprBlock)` if an `else` block exists, otherwise `None`.
 pub fn get_else_expr_block(else_clause: OptionElseClause, db: &dyn SyntaxGroup) -> Option<ExprBlock> {
     match else_clause {
         OptionElseClause::Empty(_) => None,
