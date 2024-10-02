0094b6a9f8d3348a5a4438ce70443235f31a6e4f
Charlotte <49371958+chachaleo@users.noreply.github.com>
2024-10-02 23:19:53 +0800

    feat: manual_ok and manual_err (#133)
    * feat: manual_ok and manual_err
    
    * fix review
    
    * small cleaning

--- crates/cairo-lint-core/src/lints/manual/helpers.rs
+++ crates/cairo-lint-core/src/lints/manual/helpers.rs
@@ -53,6 +53,39 @@ pub fn pattern_check_enum_arg(pattern: &Pattern, db: &dyn SyntaxGroup, arg_name:
     }
 }
 
+/// Checks if the given `Expr::FunctionCall` expression matches the input `enum_name` and
+/// verifies that the first argument of the function call corresponds to pattern enum arg.
+///
+/// # Arguments
+/// * `expr` - The expression to check, expected to be a function call.
+/// * `pattern` - The pattern to match against the function's first argument.
+/// * `db` - Reference to the `SyntaxGroup` for accessing the syntax tree.
+/// * `enum_name` - The name of the enum to match against the function call.
+///
+/// # Returns
+/// * `true` if the expression is a the input `enum_name` enum and the first argument matches the
+///   input pattern enum arg, otherwise `false`.
+pub fn pattern_check_enum_arg_is_expression(
+    expr: Expr,
+    pattern: Pattern,
+    db: &dyn SyntaxGroup,
+    enum_name: String,
+) -> bool {
+    if let Expr::FunctionCall(func_call) = expr {
+        if func_call.path(db).as_syntax_node().get_text(db) == enum_name {
+            pattern_check_enum_arg(
+                &pattern,
+                db,
+                func_call.arguments(db).arguments(db).elements(db)[0].as_syntax_node().get_text(db),
+            )
+        } else {
+            false
+        }
+    } else {
+        false
+    }
+}
+
 /// Checks if the inner_pattern in the input `Pattern::Enum` matches the given expr
 ///
 /// # Arguments
@@ -143,6 +176,57 @@ pub fn get_else_expr_block(else_clause: OptionElseClause, db: &dyn SyntaxGroup) 
             _ => None,
         },
     }
+}
+
+/// Checks if the condition of the input `ExprIf` expression contains an enum pattern and contains
+/// an inner pattern that matches the inner pattern of the enum in the if block.
+///
+/// # Arguments
+/// * `expr` - The `ExprIf` expression containing the condition and the if block to check.
+/// * `db` - Reference to the `SyntaxGroup` for accessing the syntax tree.
+/// * `enum_name` - The name of the enum to match against the function call in the if block.
+///
+/// # Example
+/// for :
+/// if let Enum(x) = res_val {
+///     EnumName(x)
+/// }
+/// checks that x == x
+///
+/// # Returns
+/// * `true` if the inner pattern of the enum in the condition matches the first argument of the
+///   enum `enume_name` in the statement of the if block, otherwise `false`.
+pub fn expr_check_condition_enum_inner_pattern_is_if_block_enum_inner_pattern(
+    expr: &ExprIf,
+    db: &dyn SyntaxGroup,
+    enum_name: String,
+) -> bool {
+    let Condition::Let(condition_let) = expr.condition(db) else {
+        return false;
+    };
+
+    let Pattern::Enum(enum_pattern) = &condition_let.patterns(db).elements(db)[0] else {
+        return false;
+    };
+
+    let OptionPatternEnumInnerPattern::PatternEnumInnerPattern(inner_pattern) = enum_pattern.pattern(db) else {
+        return false;
+    };
+
+    let Statement::Expr(statement_expr) = expr.if_block(db).statements(db).elements(db)[0].clone() else {
+        return false;
+    };
+
+    let Expr::FunctionCall(func_call) = statement_expr.expr(db) else {
+        return false;
+    };
+
+    if func_call.path(db).as_syntax_node().get_text_without_trivia(db) != enum_name {
+        return false;
+    }
+
+    func_call.arguments(db).arguments(db).elements(db)[0].as_syntax_node().get_text_without_trivia(db)
+        == inner_pattern.pattern(db).as_syntax_node().get_text_without_trivia(db)
 }
 
 /// Checks if the input `Expr` is a default of the expr kind.
